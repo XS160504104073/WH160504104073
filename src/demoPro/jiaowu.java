236957951b2670f25e89b76751dfeda55cd965f3
Administrator <Administrator@MD501-36>
2019-05-21 14:30:48 +0800

    123

---
+++ src/demoPro/jiaowu.java
@@ -0,0 +1,6 @@
+	package demoPro;
+
+public class jiaowu {
+	public static void main(String[] args) {
+		System.out.println("good!");
+}
---
+++ src/demoPro/student.java
@@ -0,0 +1,10 @@
+package demoPro;
+
+public class student {
+	public String name;
+	   public String address;
+	   
+	   student(){
+		   
+	   }
+}
